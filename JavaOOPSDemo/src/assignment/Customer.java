dcaf57082a1d93569b2da36cd555c72e0caef18a
Shalini Mittal <shalini06.mittal@gmail.com>
2024-02-04 15:41:27 +0530

    oops assignment

---
+++ JavaOOPSDemo/src/assignment/Customer.java
@@ -0,0 +1,15 @@
+package assignment;
+
+/**
+ * Create a class Customer with following data members
+ * firstname, lastname, emailid, phone, password
+ *
+ * Create a method to initialize the firstname, lastname, phone and password.
+ * Create a method to generate email id using the below pattern
+ * firstname.lastname@gmail.com. Call this method from initialize() to initialize the email id
+ *
+ * Create a method displayDetails(), that display the details of the customer excluding password
+ * Create a Test class, create a customer object and call respective methods
+ */
+public class Customer {
+}
